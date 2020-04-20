c88ada2dfccf8a1190e34a745dd21dac03bd6541
Jason Marsh <regularly.jason@gmail.com>
2020-04-19 21:06:39 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Intermittent-Aeration
  A small project experimenting with DWC Hydroponics run intermittently
+
+This project primarily exists as a learning exercise in electronics, soldering, working with batteries and solar panels etc.  
+
+## Notes
+
+Currently power usage is unnecessarily inefficient, there is no sleep sleep mode, just the trinket sitting there the majority of the time counting until the next time it gets to cycle the pump.
--- code.py
+++ code.py
@@ -1,33 +1,39 @@
 import board
 import time
 import simpleio
-import pulseio
+#import pulseio
 
-buzzer = pulseio.PWMOut(board.D3, frequency=5000, duty_cycle=0)
-pump = simpleio.DigitalOut(board.D13)
+#buzzer = pulseio.PWMOut(board.D3, frequency=2500, duty_cycle=0)
+pump = simpleio.DigitalOut(board.D0)
 
 def cyclePump():
     # Ultimately the timing for on/off will be more like 30 on and 3600 off.
     pump.value = True
-    time.sleep(1)
+    time.sleep(15)
     pump.value = False
 
-def makeBeep():
-    #mostly for learning purposes, but emits a quick beep pattern - disable when running via battery?
-    buzzer.duty_cycle = 65535
-    time.sleep(0.08)
-    buzzer.duty_cycle = 0
-    time.sleep(0.1)
-    buzzer.duty_cycle = 65535
-    time.sleep(0.08)
-    buzzer.duty_cycle = 0
+#def makeBeep():
+#    #mostly for learning purposes, but emits a quick beep pattern - disable when running via battery?
+#    buzzer.duty_cycle = 30000
+#    time.sleep(0.08)
+#    buzzer.duty_cycle = 65535
+#    time.sleep(0.1)
+#    buzzer.duty_cycle = 30000
+#    time.sleep(0.08)
+#    buzzer.duty_cycle = 0
+
 
 def generateO2():
-    makeBeep()
+#    makeBeep()
     cyclePump()
 
 while True:
     generateO2()
-    time.sleep(10)
+    time.sleep(3600)
 
-
+#while True:
+#    print("Oxygenating the water!")
+#    generateO2()
+#    for number in range(3600, 0, -1):
+#        time.sleep(1)
+#        print("Next pump cycle will begin in " + str(number) + " seconds.")
